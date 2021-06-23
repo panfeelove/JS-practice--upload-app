f88dabb876b28902aaba5bf8fa3a139fe99708c0
Tim Panfilov <tpanfilov.dev@gmail.com>
2021-06-23 16:57:39 +0300

    Upload plugin v0.2 : Added remove functionality

--- upload.js
+++ upload.js
@@ -1,4 +1,13 @@
+function bytesToSize(bytes) {
+    var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+    if (bytes == 0) return '0 Byte';
+    var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+    return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i];
+ }
+
+
 export function upload(selector , options={}){
+    let files = [];
     const input = document.querySelector(selector);
     const preview = document.createElement('div');
     preview.classList.add('preview');
@@ -9,32 +18,33 @@ export function upload(selector , options={}){
     if(options.multi){
         input.setAttribute('multiple',true);
     }
-
     if(options.accept && Array.isArray(options.accept)){
         input.setAttribute('accept', options.accept.join(','))
     }
-
     input.insertAdjacentElement('afterend',preview);
     input.insertAdjacentElement('afterend',open);
-
     const triggerInput = ()=>{
         input.click();
     }
 
     const changeHandler= (event)=>{
         if(!event.target.files.length) return;
-        const files = Array.from(event.target.files);
+        files = Array.from(event.target.files);
+        preview.innerHTML ="";
         files.forEach(file =>{
             if(!file.type.match('image'))return;
-
             const reader = new FileReader();
 
             reader.onload = ev =>{
                 const src = ev.target.result;
                 preview.insertAdjacentHTML('afterbegin',`
                     <div class='preview-image'>
+                        <div class="preview-remove" data-name="${file.name}">&times;</div>
                         <img src="${src}" alt="${file.name}">
-
+                        <div class="preview-info">
+                            <span>${file.name}</span>
+                            ${bytesToSize(file.size)}
+                        </div>
                     </div>
                 `)
             }
@@ -42,6 +52,18 @@ export function upload(selector , options={}){
         });
     }
 
+    const removeHandler = event =>{
+        if(!event.target.dataset.name)return;
+        const {name} = event.target.dataset;
+        files = files.filter(file => file.name !== name);
+        const block = preview.querySelector(`[data-name ="${name}"]`)
+        .closest('.preview-image');
+        block.classList.add('removing');
+        setTimeout(()=>block.remove(),300)
+        // block.remove();
+    }
+
     open.addEventListener('click',triggerInput);
     input.addEventListener('change',changeHandler);
+    preview.addEventListener('click',removeHandler)
 }
