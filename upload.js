c92565cbf1d82ad56bfe01c1eb7cbd118ceb2eec
Tim Panfilov <tpanfilov.dev@gmail.com>
2021-06-23 16:15:31 +0300

    created upload-plugin v0.1

---
+++ upload.js
@@ -0,0 +1,47 @@
+export function upload(selector , options={}){
+    const input = document.querySelector(selector);
+    const preview = document.createElement('div');
+    preview.classList.add('preview');
+    const open = document.createElement('button');
+    open.classList.add('btn');
+    open.textContent = 'Open';
+
+    if(options.multi){
+        input.setAttribute('multiple',true);
+    }
+
+    if(options.accept && Array.isArray(options.accept)){
+        input.setAttribute('accept', options.accept.join(','))
+    }
+
+    input.insertAdjacentElement('afterend',preview);
+    input.insertAdjacentElement('afterend',open);
+
+    const triggerInput = ()=>{
+        input.click();
+    }
+
+    const changeHandler= (event)=>{
+        if(!event.target.files.length) return;
+        const files = Array.from(event.target.files);
+        files.forEach(file =>{
+            if(!file.type.match('image'))return;
+
+            const reader = new FileReader();
+
+            reader.onload = ev =>{
+                const src = ev.target.result;
+                preview.insertAdjacentHTML('afterbegin',`
+                    <div class='preview-image'>
+                        <img src="${src}" alt="${file.name}">
+
+                    </div>
+                `)
+            }
+            reader.readAsDataURL(file);
+        });
+    }
+
+    open.addEventListener('click',triggerInput);
+    input.addEventListener('change',changeHandler);
+}
